af014650397af9e6ab0106435ea05e176032b729
Author.zero <1090863704@qq.com>
2019-04-14 11:08:17 +0800

    inti commit

---
+++ src/main/java/com/zero/ytoken/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.zero.ytoken.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @classname: EurekaApplication
+ * @description: TODO
+ * @author: Author.zero
+ * @create: 2019-04-14 11:06
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
